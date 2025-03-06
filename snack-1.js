21e5d60059f7f91a721008521e776959d63ebf68
Arela-Cristina <instagrandine@gmail.com>
2025-03-06 19:34:36 +0100

    feat: first snack done

---
+++ snack-1.js
@@ -0,0 +1,35 @@
+// 🏆 Snack 1
+// Ottieni il titolo di un post con una Promise.
+
+// Crea una funzione getPostTitle(id) che accetta un id e restituisce una Promise che recupera il titolo di un post dal link https://dummyjson.com/posts/{id}
+// 🎯 Bonus: Ottieni l'intero post con l'autore
+// Crea una funzione getPost(id) che recupera l'intero post. Concatena una seconda chiamata che aggiunge una proprietà user che contiene i dati dell'autore, recuperati dalla chiamata https://dummyjson.com/users/{post.userId}.
+
+
+
+
+
+function getPostTitle(id) {
+    const promisse = new Promise((resolve, reject) => {
+
+        console.log('Sto esseguendo la chiamata...');
+
+        fetch(`https://dummyjson.com/posts/${id}`)
+            .then(response => response.json())
+            .then(data => resolve(data))
+            .catch(error => reject(error))
+
+    });
+
+    return promisse
+}
+
+
+getPostTitle(1)
+    .then(data => console.log(data.title))
+    .catch(error => console.log(error))
+
+
+
+
+    
